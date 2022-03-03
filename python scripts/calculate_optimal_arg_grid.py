65282fdae40d0953dc12b68538c1ca5ef82e08ae
mcolpus <matthew@colpus.co.uk>
2022-03-03 11:19:11 +0000

    Calculate optimal arg grids

--- python scripts/calculate_optimal_arg_grid.py
+++ python scripts/calculate_optimal_arg_grid.py
@@ -4,7 +4,7 @@ import numpy as np
 import sys
 import getopt
 import matplotlib as mpl
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 from typing import List, Dict, Tuple
 
 
@@ -26,9 +26,10 @@ def read_args(inputfile):
 
         values = line.split(',')
         recombinations = int(values[0])
-        back_mutations = int(values[1])
+        back_mutations = int(values[1]) # On kwarg this is sequential errors
         recurrent_mutations = int(values[2])
-        arg_runs.append((recombinations, back_mutations, recurrent_mutations))
+        if (recombinations != -1):
+            arg_runs.append((recombinations, back_mutations, recurrent_mutations))
 
     file.close()
 
@@ -71,26 +72,116 @@ def calculate_optimal_grid(fewest_rms, max_recombs, max_bms):
                 grid[i, j] = min(grid[i, j], grid[i-1, j])
             elif j > 0:
                 grid[i, j] = min(grid[i, j], grid[i, j-1])
+    
+    for i in range(max_recombs+1):
+        for j in range(max_bms+1):
+            if grid[i, j] == sys.maxsize:
+                grid[i, j] = -1
 
     return grid
 
 
 def graph_grid(grid, outputfile):
-    fig = pyplot.figure(1)
+    fig = plt.figure(1)
 
-    cmap2 = mpl.colors.LinearSegmentedColormap.from_list('my_colormap',
-                                                         ['green', 'blue', 'red'],
-                                                         256)
+    cmap = mpl.colors.LinearSegmentedColormap.from_list('my_colormap',
+                                                        ['green', 'blue', 'red'],
+                                                        np.max(grid) - np.min(grid) + 1)
 
-    img2 = pyplot.imshow(grid, interpolation='nearest',
-                         cmap=cmap2, aspect='auto',
-                         origin='lower')
+    img = plt.imshow(grid.transpose(), interpolation='nearest',
+                     cmap=cmap, aspect='auto', origin='lower')
 
-    pyplot.colorbar(img2, cmap=cmap2)
+    plt.colorbar(img, cmap=cmap)
 
-    pyplot.show()
+    plt.title(
+        'Fewest recurrent-mutations required \n given recombination and back-mutations')
+    plt.xlabel('Recombinations')
+    plt.ylabel('Back-mutations')
 
-    fig.savefig(outputfile)
+    plt.show()
+
+    fig.savefig(outputfile + "_grid.png")
+
+
+def graph_all_recombs_vs_rare_mutations(arg_runs, outputfile):
+    fig = plt.figure(2)
+
+    recombinations = []
+    rare_muts = []
+    max_recombs = 0
+    for (recombs, bms, rms) in arg_runs:
+        recombinations.append(recombs)
+        rare_muts.append(bms + rms)
+        max_recombs = max(max_recombs, recombs)
+
+    fewest_rare_muts = [sys.maxsize for i in range(max_recombs+1)]
+    for (recombs, bms, rms) in arg_runs:
+        fewest_rare_muts[recombs] = min(fewest_rare_muts[recombs], bms+rms)
+
+    for i in range(1, max_recombs+1):
+        fewest_rare_muts[i] = min(fewest_rare_muts[i-1], fewest_rare_muts[i])
+
+    plt.plot(range(max_recombs+1), fewest_rare_muts,
+             color='red', label="optimal")
+    plt.scatter(recombinations, rare_muts, color='blue',
+                label='all networks created')
+
+    plt.title('Recombinations vs Rare Mutations \n for all args created')
+    plt.xlabel('Recombinations')
+    plt.ylabel('recurrent + back mutations')
+    plt.legend(loc="upper right")
+
+    # plt.show()
+
+    fig.savefig(outputfile + "_rare_muts.png")
+
+def graph_comparison(arg_runs1, arg_runs2, outputfile):
+    fig = plt.figure(3)
+
+    recombinations1 = []
+    rare_muts1 = []
+    max_recombs = 0
+    for (recombs, bms, rms) in arg_runs1:
+        recombinations1.append(recombs)
+        rare_muts1.append(bms + rms)
+        max_recombs = max(max_recombs, recombs)
+
+    recombinations2 = []
+    rare_muts2 = []
+    for (recombs, bms, rms) in arg_runs2:
+        recombinations2.append(recombs)
+        rare_muts2.append(bms + rms)
+        max_recombs = max(max_recombs, recombs)
+
+    fewest_rare_muts1 = [sys.maxsize for i in range(max_recombs+1)]
+    for (recombs, bms, rms) in arg_runs1:
+        fewest_rare_muts1[recombs] = min(fewest_rare_muts1[recombs], bms+rms)
+    for i in range(1, max_recombs+1):
+        fewest_rare_muts1[i] = min(fewest_rare_muts1[i-1], fewest_rare_muts1[i])
+    
+    fewest_rare_muts2 = [sys.maxsize for i in range(max_recombs+1)]
+    for (recombs, bms, rms) in arg_runs2:
+        fewest_rare_muts2[recombs] = min(fewest_rare_muts2[recombs], bms+rms)
+    for i in range(1, max_recombs+1):
+        fewest_rare_muts2[i] = min(fewest_rare_muts2[i-1], fewest_rare_muts2[i])
+
+    plt.plot(range(max_recombs+1), fewest_rare_muts1,
+             color='red', label="optimal 1")
+    plt.plot(range(max_recombs+1), fewest_rare_muts2,
+             color='blue', label="optimal 2")
+    plt.scatter(recombinations1, rare_muts1, color='green', alpha=0.5,
+                label='all networks created 1')
+    plt.scatter(recombinations2, rare_muts2, color='yellow', alpha=0.5,
+                label='all networks created 2')
+
+    plt.title('Recombinations vs Rare Mutations \n comparison')
+    plt.xlabel('Recombinations')
+    plt.ylabel('recurrent + back mutations')
+    plt.legend(loc="upper right")
+
+    # plt.show()
+
+    fig.savefig(outputfile + ".png")
 
 
 def main(argv):
@@ -114,16 +205,24 @@ def main(argv):
     print('Input file is: ', inputfile)
     print('Output file is: ', outputfile)
 
-    arg_runs = read_args(inputfile)
-    (fewest_rms, max_recombs, max_bms) = runs_to_dict(arg_runs)
-    grid = calculate_optimal_grid(fewest_rms, max_recombs, max_bms)
+    if ',' in inputfile:
+        inputfile1, inputfile2 = inputfile.split(',')
+        arg_runs1 = read_args(inputfile1)
+        arg_runs2 = read_args(inputfile2)
+        graph_comparison(arg_runs1, arg_runs2, outputfile)
+    else:
+        arg_runs = read_args(inputfile)
+        (fewest_rms, max_recombs, max_bms) = runs_to_dict(arg_runs)
+        grid = calculate_optimal_grid(fewest_rms, max_recombs, max_bms)
 
-    graph_grid(grid, outputfile)
+        graph_grid(grid, outputfile)
 
-    print(max_recombs)
-    print(max_bms)
-    print(fewest_rms)
-    print(grid)
+        graph_all_recombs_vs_rare_mutations(arg_runs, outputfile)
+
+        print(max_recombs)
+        print(max_bms)
+        print(fewest_rms)
+        print(grid)
 
 
 if __name__ == "__main__":
