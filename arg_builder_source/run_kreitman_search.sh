f862cf3640226101ad047640747259a97f10a8c7
mcolpus <matthew@colpus.co.uk>
2022-03-03 11:18:43 +0000

    bash scripts for running searches

--- arg_builder_source/run_kreitman_search.sh
+++ arg_builder_source/run_kreitman_search.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for r in 0.4 0.6 0.9 1.1 1.5 1.9 2.1
+for r in 0.5 1.0 1.5 2.0 2.5
 do
-    for b in 1.1 1.5 1.9 2.1
+    for b in 1.0 1.1 1.5 2.0 
     do
-        ./arg_builder -l -V1 -c -oauto_kreitman_records.csv -Q10 -R${r} -B${b} < kreitman_rooted_ordered.txt
+        ./arg_builder -l -V1 -c -othreaded_kreitman_records.csv -L1 -Q5 -F3 -R${r} -B${b} < kreitman_labelled.txt
     done
 done
--- arg_builder_source/run_sample_search.sh
+++ arg_builder_source/run_sample_search.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./arg_builder -l -V1 -c -osample_records.csv -r1 -R0.4,0.6,0.9,1.0,1.1,1.5,1.9,2.1,2.5,3.0,50.0 -B1.1,1.3,1.5,1.9,2.1,2.9,3.1,50.0 -Q20 < samplerefbin500.txt
+./arg_builder -l -V1 -c -L3 -oselection3.csv -r1 -R0.5,0.8,1.0,1.5,2.0 -B1.1,1.5,2.0 -Q10 < samplerefA500.txt
