0ee19c8835fa4f196d85dd0d93b968ac7f4d68d6
mcolpus <matthew@colpus.co.uk>
2022-03-14 22:32:24 +0000

    prepare final covid experiments

--- covid_experiments/run_kwarg_medium_samples.sh
+++ covid_experiments/run_kwarg_medium_samples.sh
@@ -2,11 +2,11 @@
 
 for j in 200 500
 do
-    for i in A B C 
+    for i in A C B 
     do
-        ../source/kwarg -T15 -Q10 -k -Okwarg_records_sample${i}${j}.csv -S-1,1.5,1.2,1.1,1.0,0.9,0.8,0.7,0.5,1 \
-                                                                        -M-1,1.51,1.21,1.11,1.01,0.91,0.81,0.71,0.51,1.1 \
-                                                                        -R1,1,1,1,1,1,1,1,1,-1 -C2,2,2,2,2,2,2,2,2,-1 < sample${i}${j}.txt
+        ../source/kwarg -T15 -Q10 -k -r600 -Okwarg_records_sample${i}${j}.csv -S-1,1.5,1.2,1.1,1.0,0.9,0.8,0.7,0.5,1 \
+                                                                              -M-1,1.51,1.21,1.11,1.01,0.91,0.81,0.71,0.51,1.1 \
+                                                                              -R1,1,1,1,1,1,1,1,1,-1 -C2,2,2,2,2,2,2,2,2,-1 < sample${i}${j}.txt
         echo "completed kwarg ${i}${j}"
     done
 done
--- covid_experiments/run_kwarg_tricky.sh
+++ covid_experiments/run_kwarg_tricky.sh
@@ -4,7 +4,7 @@ for j in 50
 do
     for i in B 
     do
-        ../source/kwarg -T50 -Q20 -k -Otricky_sample${i}${j}.csv -S0.7 -M0.71 -R1 -C2 -r10 < sample${i}${j}.txt
+        ../source/kwarg -T50 -Q20 -k -Otricky_sample${i}${j}.csv -S0.7 -M0.71 -R1 -C2 -r4 < sample${i}${j}.txt
         echo "completed kwarg ${i}${j}"
     done
 done
